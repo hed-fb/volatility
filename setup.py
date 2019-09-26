0088dae0aab8eb5685a89402adfa1ddf6be76c02
hed-fb <49454131+hed-fb@users.noreply.github.com>
2019-09-26 10:01:13 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,32 +21,17 @@
 # You should have received a copy of the GNU General Public License
 # along with Volatility.  If not, see <http://www.gnu.org/licenses/>.
 #
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="volatility"
-    version="2.6.2"
+    version="2.7"
     description="Volatility -- Volatile memory framework"
     author="AAron Walters"
     author_email="awalters@4tphi.net"
     url="http://www.volatilityfoundation.org"
     license= "GPL"
     scripts= ["vol.py"]
-    packages=["volatility",
-        "volatility.win32",
-        "volatility.renderers",
-        "volatility.plugins",
-        "volatility.plugins.addrspaces",
-        "volatility.plugins.overlays",
-        "volatility.plugins.overlays.windows",
-        "volatility.plugins.overlays.linux",
-        "volatility.plugins.overlays.mac",
-        "volatility.plugins.gui",
-        "volatility.plugins.gui.vtypes",
-        "volatility.plugins.linux",
-        "volatility.plugins.registry",
-        "volatility.plugins.malware", 
-        "volatility.plugins.mac",
-    ]
-    install_requires=['socket', 'ctypes', 'Crypto.Cipher', 'urllib', 'distorm3', 'yara', 'xml.etree.ElementTree']
+    packages=find_packages()
+    install_requires=['pycrypto>2.5','distorm3>3.3.3', 'yara-python>3']
 )
